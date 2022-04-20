6600fcb3b36daf7ddb8d6d286ca9baa45a728b7e
Aaron Gibson <asgibsonua@zoho.com>
2022-04-19 22:05:44 -0700

    Creating MultipartFormDataParser and delegate.

--- tests/file_util_test.py
+++ tests/file_util_test.py
@@ -7,10 +7,14 @@ import os
 import shutil
 import tempfile
 # Third-party imports.
-from tornado import web, httpclient
-from tornado.testing import AsyncHTTPTestCase, gen_test
+from tornado import web, httpclient, httputil
+from tornado.testing import AsyncTestCase, AsyncHTTPTestCase, gen_test
 # Local imports
-from torncoder.file_util import BasicStaticFileHandler
+from torncoder.file_util import (
+    BasicStaticFileHandler,
+    # For the MultipartFormData parsing tests.
+    MultipartFormDataParser
+)
 
 # Try optional imports and flag which ones were successful. If an import
 # succeeds, we should run the additional tests for that section.
@@ -44,10 +48,10 @@ class Base:
             with open(path, 'wb') as stm:
                 stm.write(b'a' * 1024)
 
-            path = os.path.join(cls.temp_dir, 'B.data')
-            with open(path, 'wb') as stm:
-                for i in range(1000000):
-                    stm.write(b'b')
+            # path = os.path.join(cls.temp_dir, 'B.data')
+            # with open(path, 'wb') as stm:
+            #     for i in range(1000000):
+            #         stm.write(b'b')
 
         @classmethod
         def tearDownClass(cls):
@@ -131,6 +135,153 @@ if AIO_IMPORTED:
             return web.Application([
                 (r'/(.+)', BasicAIOFileHandler, dict(root_path=self.temp_dir))
             ])
+
+#
+# FormData Parsing Test Cases
+#
+MULTIPART_DATA = b"""----boundarything\r
+Content-Disposition: form-data; name="a.txt"\r
+\r
+a----boundarything\r
+Content-Disposition: form-data; name=b.csv"\r
+Content-Type: text/csv\r
+\r
+col1,col2
+a,b
+--boundarythin,thatwasclose
+----boundarything--\r
+"""
+
+
+class BasicDelegate(object):
+
+    def __init__(self):
+        self.parsed_data = dict()
+        self.parsed_info = dict()
+        self.finished_files = []
+
+    def start_new_file(self, name, headers):
+        self.parsed_info[name] = headers
+
+    def file_data_received(self, name, data):
+        if name not in self.parsed_data:
+            self.parsed_data[name] = bytearray(data)
+        else:
+            self.parsed_data[name].extend(data)
+
+    def finish_file(self, name):
+        self.finished_files.append(name)
+
+
+class BasicAsyncDelegate(object):
+
+    def __init__(self):
+        self.parsed_data = dict()
+        self.parsed_info = dict()
+
+    async def start_new_file(self, name, headers):
+        self.parsed_info[name] = headers
+
+    async def file_data_received(self, name, data):
+        if name not in self.parsed_data:
+            self.parsed_data[name] = bytearray(data)
+        else:
+            self.parsed_data[name].extend(data)
+
+    async def finish_file(self, name):
+        self.finished_files.append(name)
+
+
+class StreamFormData(AsyncTestCase):
+
+    @gen_test
+    async def test_multipart_form_data(self):
+        boundary = b'--boundarything'
+
+        headers_a_txt = list(httputil.HTTPHeaders({
+            'Content-Disposition': 'form-data; name="a.txt"',
+        }).get_all())
+        headers_b_csv = list(httputil.HTTPHeaders({
+            'Content-Disposition': 'form-data; name=b.csv',
+            'Content-Type': 'text/csv;'
+        }))
+
+        # Test all possible splits and chunks of the given data. This will
+        # verify the parser with all possible corner cases.
+        for i in range(len(MULTIPART_DATA)):
+            delegate = BasicDelegate()
+            parser = MultipartFormDataParser(delegate, boundary)
+            chunk1 = MULTIPART_DATA[:i]
+            chunk2 = MULTIPART_DATA[i:]
+            await parser.data_received(chunk1)
+            await parser.data_received(chunk2)
+
+            # Verify that the delegate contents are correct.
+            self.assertEqual(
+                set(['a.txt', 'b.csv']), set(delegate.parsed_data.keys()),
+                "Expected files not found for slicing at: {}".format(i))
+            # Assert the 'headers' match what is expected.
+            self.assertEqual(
+                headers_a_txt,
+                list(delegate.parsed_info['a.txt'].get_all()),
+                '"a.txt" header mismatch on slice: {}'.format(i))
+            self.assertEqual(
+                headers_b_csv,
+                list(delegate.parsed_info['b.csv']),
+                '"b.csv" header mismatch on slice: {}'.format(i))
+            # Assert that the file contents match what is expected.
+            self.assertEqual(
+                b'a', bytes(delegate.parsed_data['a.txt']),
+                '"a.txt" file contents mismatch on slice: {}'.format(i))
+            self.assertEqual(
+                b'col1,col2\na,b\n--boundarythin,thatwasclose\n',
+                bytes(delegate.parsed_data['b.csv']),
+                '"b.csv" file contents mismatch on slice: {}'.format(i))
+
+    @gen_test
+    async def test_multipart_form_data_async(self):
+        # Same test as above, but with async methods for the delegate.
+        boundary = b'--boundarything'
+
+        headers_a_txt = list(httputil.HTTPHeaders({
+            'Content-Disposition': 'form-data; name="a.txt"',
+        }).get_all())
+        headers_b_csv = list(httputil.HTTPHeaders({
+            'Content-Disposition': 'form-data; name=b.csv',
+            'Content-Type': 'text/csv;'
+        }))
+
+        # Test all possible splits and chunks of the given data. This will
+        # verify the parser with all possible corner cases.
+        for i in range(len(MULTIPART_DATA)):
+            delegate = BasicAsyncDelegate()
+            parser = MultipartFormDataParser(delegate, boundary)
+            chunk1 = MULTIPART_DATA[:i]
+            chunk2 = MULTIPART_DATA[i:]
+            await parser.data_received(chunk1)
+            await parser.data_received(chunk2)
+
+            # Verify that the delegate contents are correct.
+            self.assertEqual(
+                set(['a.txt', 'b.csv']), set(delegate.parsed_data.keys()),
+                "Expected files not found for slicing at: {}".format(i))
+            # Assert the 'headers' match what is expected.
+            self.assertEqual(
+                headers_a_txt,
+                list(delegate.parsed_info['a.txt'].get_all()),
+                '"a.txt" header mismatch on slice: {}'.format(i))
+            self.assertEqual(
+                headers_b_csv,
+                list(delegate.parsed_info['b.csv']),
+                '"b.csv" header mismatch on slice: {}'.format(i))
+            # Assert that the file contents match what is expected.
+            self.assertEqual(
+                b'a', bytes(delegate.parsed_data['a.txt']),
+                '"a.txt" file contents mismatch on slice: {}'.format(i))
+            self.assertEqual(
+                b'col1,col2\na,b\n--boundarythin,thatwasclose\n',
+                bytes(delegate.parsed_data['b.csv']),
+                '"b.csv" file contents mismatch on slice: {}'.format(i))
 
 
 if __name__ == '__main__':
